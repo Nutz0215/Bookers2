a3568b3c1602a843f35bbccce80bf0673d63118d
EC2 Default User <ec2-user@ip-172-31-17-130.ec2.internal>
2022-12-10 14:21:14 +0000

    add favorites and comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about' => 'homes#about',as: 'about'
   get '/users' => 'users#alluser',as: 'alluser'
-  resources :books, only: [:new, :index, :show, :create, :destroy, :edit, :update]
+  resources :books, only: [:new, :index, :show, :create, :destroy, :edit, :update] do
+    resources :book_comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
+  end
   resources :users, only: [:edit, :show, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
